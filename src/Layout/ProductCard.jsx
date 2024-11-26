e669c4b8530487ba25fb7caf2b09ee299b458d30
rakibrockz <rhossan1064@gmail.com>
2024-11-26 21:48:19 +0600

    updated responsive2

--- src/Layout/ProductCard.jsx
+++ src/Layout/ProductCard.jsx
@@ -3,7 +3,7 @@ import { FiArrowRight } from "react-icons/fi";
 
 const ProductCard = ({ className,Price, src, detail}) => {
   return (
-    <div className={` group md:w-[250px] sm:w-[220px] mx-2 w-[150px] md:mx-0 ${className} bg-white rounded-2xl overflow-hidden hover:scale-105 transition-all duration-200`}>
+    <div className={` group md:w-[250px] sm:w-[220px] mx-2 w-[185px] md:mx-0 ${className} bg-white rounded-2xl overflow-hidden hover:scale-105 transition-all duration-200`}>
       <div className="">
         <img className="w-full group-hover:scale-105 transition-all duration-500" src={src} alt="" />
       </div>
